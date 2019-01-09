c34296c6cf7a356d84bfd7db876b6dc00557974c
Arthur Hallier <tutur.77@hotmail.fr>
2019-01-10 00:42:22 +0100

    améliorations fichier config

--- Adopte-un-poumon/functions/config_files.c
+++ Adopte-un-poumon/functions/config_files.c
@@ -7,39 +7,32 @@ void changes_files(GtkWidget * mainWindow)
 {
     char line [50]; // Maximum de caractere sur une line
     char * array;
-    int theCurrentChar;
+    char * find_equal;
+    int actual_carac = 0;
     int cursor=0;
+    int i=0;
     FILE* open_file;
     int size=0;
     open_file = fopen("config/config.txt","r+");
     if (open_file !=NULL)
     {
-        /*
-        fseek(open_file,0,SEEK_END);
-        size =ftell(open_file);
-        printf("%d \n",size);
-        fseek(open_file,0,SEEK_SET);
-        array = malloc(sizeof(char)*size);
+        find_equal = malloc(sizeof(char)* 50);
 
-        // LECTURE DU CHEMIN DE L'IMAGE
-        fseek(open_file,12,SEEK_SET);
-
-        fread(array,sizeof(char),size/sizeof(char),open_file);
-        */
-        cursor= ftell(open_file);
         while ( fgets ( line, sizeof(line), open_file ) != NULL ) // LIS LE FICHIER LIGNE PAR LIGNE
             // RAJOUTE /n et /O a la fin de chaque ligne sauf la derniere
         {
-            if (strstr(line,"=") != NULL)
-            {
-                printf("Ok");
-            }
-            printf("Ligne : %s", line);
+
+            fseek(open_file,0,SEEK_CUR);
+            cursor = ftell(open_file);
+            // printf("%d",cursor);
+            find_equal = strstr(line,"=")+1;
+            puts(find_equal);
 
 
 
         }
         fclose( open_file );
+        free(find_equal);
     }
     else
     {
